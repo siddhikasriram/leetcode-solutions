80c74327d999d9cb53d96c7385639ed9f770f3b8
siddhikasriram <86295696+siddhikasriram@users.noreply.github.com>
2026-08-01 17:15:01 -0500

    Time: 0 ms (100.00%) | Memory: 13.1 MB (67.27%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,14 @@
+
+
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        hashmap = {}
+        for i, num in enumerate(nums): 
+            complement = target - num 
+
+            if complement in hashmap: 
+                return [hashmap[complement], i]
+
+            hashmap[num] = i
+        
