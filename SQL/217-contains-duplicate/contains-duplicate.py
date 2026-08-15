986c71cb73d101c4e77e8f3f7aac47936f5c3047
Siddhika Sriram <86295696+siddhikasriram@users.noreply.github.com>
2026-08-15 12:58:23 -0500

    Time: 22 ms (65.48%) | Memory: 28.6 MB (6.42%) - LeetSync

---
+++ SQL/217-contains-duplicate/contains-duplicate.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        d = {} 
+        for i in nums: 
+            if i in d:
+                return True
+            else:
+                d[i] = 1
+        return False
+
+        
+
+        
+
+
+        
